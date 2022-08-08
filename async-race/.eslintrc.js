ca90c6439987d2983af5c37c962c9b4f4ae56f09
Natallia Kostsina <natakostina123@gmail.com>
2022-08-08 22:08:08 +0200

    feat: add favicon, remove files from gitignore

---
+++ async-race/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.eslint.json',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'no-debugger': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'max-len': ['error', { code: 170, tabWidth: 4 }],
+    'no-case-declarations': 'off',
+  },
+};
